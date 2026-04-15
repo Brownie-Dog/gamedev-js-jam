d26a14948fdb104675a94e3fbe995ed30f1c4472
Matthew Lau <matthew.lau78@gmail.com>
2026-04-16 01:02:12 +1000

    Implement test interactable scene

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,30 +2,17 @@ using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.InputSystem;
 
+// TODO: Split this up further
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private PlayerStats _stats;
 
-    [SerializeField]
-    private Rigidbody2D _rigidBody;
+    public void OnLook(InputAction.CallbackContext context) { }
 
-    private Vector2 _movementInput = Vector2.zero;
+    public void OnPrimaryFire(InputAction.CallbackContext context) { }
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        _movementInput = context.ReadValue<Vector2>();
-    }
+    public void OnSecondaryFire(InputAction.CallbackContext context) { }
 
-    private void Awake()
-    {
-        Assert.IsNotNull(_rigidBody);
-    }
-
-    private void FixedUpdate()
-    {
-        _rigidBody.MovePosition(
-            _rigidBody.position + _movementInput * (_stats.MovementSpeed * Time.fixedDeltaTime)
-        );
-    }
+    public void OnLegButton(InputAction.CallbackContext context) { }
 }
