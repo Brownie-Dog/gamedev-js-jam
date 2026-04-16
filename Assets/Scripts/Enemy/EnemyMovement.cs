f5823ed1eae8778a592da723bc772aeabb79d64e
Swxer <swxergamer64@gmail.com>
2026-04-17 00:16:45 +1000

    Implement basic enemy movement

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -2,15 +2,57 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Movement Settings")]
+    [SerializeField]
+    private float _movementSpeed = 5f;
+    [SerializeField]
+    private float _detectionRadius = 5.0f;
+    
+    [Header("Audio Wrapper")]
+    [SerializeField]
+    private SoundEffect _detectionSound;
 
-    // Update is called once per frame
+    [Header("References")] 
+    [SerializeField]
+    private Transform _playerTransform;
+    
+    private Rigidbody2D _rb;
+    private AudioSource _audioSource;
+    
+    void Start() 
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        _audioSource = GetComponent<AudioSource>();
+    }
+    
     void Update()
     {
-        
+        float distanceToPlayer = Vector2.Distance(_playerTransform.position, transform.position);
+
+        if (IsPlayerInRange(distanceToPlayer))
+        {
+            ChasePlayer();
+            if (!_detectionSound.IsPlaying(_audioSource))
+            {
+                _detectionSound.Play(_audioSource);
+            }
+        }
+        else
+        {
+            _rb.linearVelocity = Vector2.zero;
+        }
+
+    }
+
+    private void ChasePlayer()
+    {
+        Vector2 direction = ((Vector2)_playerTransform.position - (Vector2)transform.position).normalized;
+        _rb.linearVelocity = direction *  _movementSpeed;
+    }
+
+    bool IsPlayerInRange(float distanceToPlayer)
+    {
+        return distanceToPlayer <= _detectionRadius;
     }
 }
