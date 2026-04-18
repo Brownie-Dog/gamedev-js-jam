21cd6b86f85b077df03f8e0a6314f5ac4f9423c7
Matthew Lau <matthew.lau78@gmail.com>
2026-04-19 01:05:32 +1000

    Implement health resetting and health upgrade

--- Assets/Scripts/Player/PlayerHealthUI.cs
+++ Assets/Scripts/Player/PlayerHealthUI.cs
@@ -1,15 +1,17 @@
 using System;
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.UI;
+
 public class PlayerHealthUI : MonoBehaviour
 {
-    [SerializeField] 
+    [SerializeField]
     private GameObject _HeartPrefab;
-    
+
     [SerializeField]
     private PlayerStatsSo _statsSo;
-    
+
     private List<Image> _hearts = new List<Image>();
 
     private void Awake()
@@ -20,41 +22,54 @@ public class PlayerHealthUI : MonoBehaviour
 
     private void Start()
     {
-        _statsSo.CurrentHealth = _statsSo.MaxHealth;
         SetupHearts(_statsSo.MaxHealth);
         UpdateHealthUI(_statsSo.CurrentHealth);
     }
-    
+
     private void SetupHearts(int maxHealth)
     {
         for (int i = 0; i < maxHealth; i++)
         {
-            GameObject heart = Instantiate(_HeartPrefab, transform);
-            _hearts.Add(heart.GetComponent<Image>());
+            if (i < _hearts.Count)
+            {
+                _hearts[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                GameObject heart = Instantiate(_HeartPrefab, transform);
+                _hearts.Add(heart.GetComponent<Image>());
+            }
+        }
+
+        for (int i = maxHealth; i < _hearts.Count; i++)
+        {
+            _hearts[i].gameObject.SetActive(false);
         }
     }
 
     public void UpdateHealthUI(int currentHealth)
     {
+        SetupHearts(_statsSo.MaxHealth);
+
         for (int i = 0; i < _hearts.Count; i++)
         {
             if (i < currentHealth)
             {
-                _hearts[i].color = Color.white; 
+                _hearts[i].color = Color.white;
             }
             else
             {
-                _hearts[i].color = new Color(0.5f, 0.5f, 0.5f, 0.5f); 
+                _hearts[i].color = new Color(0.5f, 0.5f, 0.5f, 0.5f);
             }
         }
     }
-    
-    private void OnEnable() 
+
+    private void OnEnable()
     {
         _statsSo.OnHealthChanged += HandleHealthUpdate;
     }
 
-    private void OnDisable() 
+    private void OnDisable()
     {
         _statsSo.OnHealthChanged -= HandleHealthUpdate;
     }
@@ -63,5 +78,4 @@ public class PlayerHealthUI : MonoBehaviour
     {
         UpdateHealthUI(_statsSo.CurrentHealth);
     }
-
 }
