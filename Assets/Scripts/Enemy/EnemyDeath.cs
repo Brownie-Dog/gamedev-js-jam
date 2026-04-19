6b3cf6c1514ee34f59204bfd49dba2a406e6a01e
Swxer <swxergamer64@gmail.com>
2026-04-19 14:47:56 +1000

    Resolve assert on awake instead of null check

--- Assets/Scripts/Enemy/EnemyDeath.cs
+++ Assets/Scripts/Enemy/EnemyDeath.cs
@@ -1,19 +1,25 @@
 using System;
+using NUnit.Framework;
 using UnityEngine;
 
 public class EnemyDeath : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private EnemyHealth _health;
-    
+
+    private void Awake()
+    {
+        Assert.IsNotNull(_health);
+    }
+
     private void OnEnable()
     {
-        if (_health != null) _health.OnDeath += HandleDeath;
+        _health.OnDeath += HandleDeath;
     }
 
     private void OnDisable()
     {
-        if (_health != null) _health.OnDeath -= HandleDeath;
+        _health.OnDeath -= HandleDeath;
     }
     
     private void HandleDeath(object sender, EventArgs e)
