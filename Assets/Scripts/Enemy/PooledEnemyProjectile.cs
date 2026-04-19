7068607a65ff8055c2555d8994a5c0b96b8250d3
Swxer <swxergamer64@gmail.com>
2026-04-19 19:38:08 +1000

    Rename object pool script and projectile for more clarity

--- Assets/Scripts/Enemy/PooledEnemyProjectile.cs
+++ Assets/Scripts/Enemy/PooledEnemyProjectile.cs
@@ -1,24 +1,26 @@
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.Pool;
 
-public class Projectile : MonoBehaviour
+public class PooledEnemyProjectile : MonoBehaviour
 {
-    [SerializeField] private float _speed = 10f;
-    private Vector2 _direction;
-    [SerializeField] private float _imageRotation = 180f;
-    [SerializeField] private float _lifetime = 5f;
+    [SerializeField] private EnemyProjectileData _data;
     
-    private IObjectPool<Projectile> _myPool;
-    public void SetPool(IObjectPool<Projectile> pool) => _myPool = pool;
+    private IObjectPool<PooledEnemyProjectile> _myPool;
     private float _timer;
     private bool _isActive;
-
+    private Vector2 _direction;
+    
+    private void Awake()
+    {
+        Assert.IsNotNull(_data);
+    }
     public void Launch(Vector2 direction)
     {
         _direction = direction.normalized;
         
         float angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, angle + _imageRotation);
+        transform.rotation = Quaternion.Euler(0, 0, angle + _data.ImageRotation);
         _timer = 0f;
         _isActive = true;
     }
@@ -26,10 +28,10 @@ public class Projectile : MonoBehaviour
     private void Update()
     {
         if (!_isActive) return;
-        transform.Translate(_direction * (_speed * Time.deltaTime), Space.World);
+        transform.Translate(_direction * (_data.Speed * Time.deltaTime), Space.World);
         
         _timer += Time.deltaTime;
-        if (_timer >= _lifetime)
+        if (_timer >= _data.Lifetime)
         {
             ReturnToPool();
         }
@@ -49,4 +51,11 @@ public class Projectile : MonoBehaviour
     {
         _myPool.Release(this);
     }
+
+    public void SetPool(IObjectPool<PooledEnemyProjectile> pool)
+    {
+        _myPool = pool;
+        
+    }
+
 }
