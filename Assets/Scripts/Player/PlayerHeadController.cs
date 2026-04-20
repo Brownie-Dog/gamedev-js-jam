e2d82bef57355d0ac715871dedf189a9bff918ec
Swxer <swxergamer64@gmail.com>
2026-04-21 04:00:53 +1000

    Refactor code to use var type

--- Assets/Scripts/Player/PlayerHeadController.cs
+++ Assets/Scripts/Player/PlayerHeadController.cs
@@ -23,16 +23,16 @@ public class PlayerHeadController : MonoBehaviour
 
     public void LookAtMouse(Vector2 mouseInput)
     {
-        Vector3 mouseWorldPos = _camera.ScreenToWorldPoint(new Vector3(mouseInput.x, mouseInput.y, 10f));
-        Vector2 direction = (Vector2)mouseWorldPos - (Vector2)transform.position;
+        var mouseWorldPos = _camera.ScreenToWorldPoint(new Vector3(mouseInput.x, mouseInput.y, 10f));
+        var direction = (Vector2)mouseWorldPos - (Vector2)transform.position;
 
         if (IsCursorTooCloseToPivot(direction)) return;
 
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         UpdateHeadRotation(angle);
     }
 
-    private bool IsCursorTooCloseToPivot(Vector2 direction)
+    private static bool IsCursorTooCloseToPivot(Vector2 direction)
     {
         const float threshold = 0.01f;
         return direction.sqrMagnitude < threshold;
@@ -40,10 +40,10 @@ public class PlayerHeadController : MonoBehaviour
 
     private void UpdateHeadRotation(float lookAngle)
     {
-        bool isLookingLeft = Mathf.Abs(lookAngle) > 90f;
+        var isLookingLeft = Mathf.Abs(lookAngle) > 90f;
         _headRenderer.flipY = isLookingLeft;
 
-        float finalRotation = isLookingLeft
+        var finalRotation = isLookingLeft
             ? ConstrainNeckRotation(lookAngle, 180f)
             : ConstrainNeckRotation(lookAngle, 0f);
 
@@ -52,8 +52,8 @@ public class PlayerHeadController : MonoBehaviour
 
     private float ConstrainNeckRotation(float currentAngle, float centerAxis)
     {
-        float angleDifference = Mathf.DeltaAngle(centerAxis, currentAngle);
-        float clampedDifference = Mathf.Clamp(angleDifference, -_maxRotationAngle, _maxRotationAngle);
+        var angleDifference = Mathf.DeltaAngle(centerAxis, currentAngle);
+        var clampedDifference = Mathf.Clamp(angleDifference, -_maxRotationAngle, _maxRotationAngle);
         return centerAxis + clampedDifference;
     }
 }
