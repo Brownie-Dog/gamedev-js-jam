af2f873c2cd0239e370dae41ed7c35786da8aa2a
Matthew Lau <matthew.lau78@gmail.com>
2026-04-24 03:53:07 +1000

    Add camera zoom for shop items

--- Assets/Scripts/Loadout/CameraController.cs
+++ Assets/Scripts/Loadout/CameraController.cs
@@ -1,5 +1,6 @@
 using Player;
 using UnityEngine;
+using UnityEngine.Assertions;
 using Weapons;
 
 namespace Loadout
@@ -14,20 +15,30 @@ namespace Loadout
 
         [SerializeField] private PlayerEquipment _playerEquipment;
 
+        [SerializeField] private float _shopFocusZoomMultiplier = 0.75f;
+
         private Camera _camera;
         private float _defaultOrthographicSize;
         private float _targetOrthographicSize;
         private Vector3 _defaultLocalPosition;
         private float _targetLocalX;
+        private float _targetLocalY;
+        private float _shopFocusZoomSize;
         private bool _loadoutOffsetActive;
+        private bool _shopFocusActive;
+        private Vector2 _shopFocusWorldPosition;
 
         private void Awake()
         {
             _camera = GetComponent<Camera>();
+            Assert.IsNotNull(_camera);
+            Assert.IsNotNull(_playerEquipment);
+
             _defaultOrthographicSize = _camera.orthographicSize;
             _targetOrthographicSize = _defaultOrthographicSize;
             _defaultLocalPosition = _camera.transform.localPosition;
             _targetLocalX = _defaultLocalPosition.x;
+            _targetLocalY = _defaultLocalPosition.y;
         }
 
         // TODO: Call this on specific events instead of every frame
@@ -35,7 +46,7 @@ namespace Loadout
         {
             var bounds = CalculatePlayerBounds();
             UpdateTargetOrthographicSize(bounds);
-            UpdateTargetLocalX();
+            UpdateTargetLocalPosition();
 
             _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, _targetOrthographicSize,
                 _zoomSpeed * Time.deltaTime
@@ -43,6 +54,7 @@ namespace Loadout
 
             var localPos = _camera.transform.localPosition;
             localPos.x = Mathf.Lerp(localPos.x, _targetLocalX, _zoomSpeed * Time.deltaTime);
+            localPos.y = Mathf.Lerp(localPos.y, _targetLocalY, _zoomSpeed * Time.deltaTime);
             _camera.transform.localPosition = localPos;
         }
 
@@ -51,8 +63,26 @@ namespace Loadout
             _loadoutOffsetActive = active;
         }
 
+        public void SetShopFocus(Vector2 worldPosition)
+        {
+            _shopFocusActive = true;
+            _shopFocusWorldPosition = worldPosition;
+            _shopFocusZoomSize = _defaultOrthographicSize * _shopFocusZoomMultiplier;
+        }
+
+        public void ClearShopFocus()
+        {
+            _shopFocusActive = false;
+        }
+
         private void UpdateTargetOrthographicSize(Bounds bounds)
         {
+            if (_shopFocusActive)
+            {
+                _targetOrthographicSize = _shopFocusZoomSize;
+                return;
+            }
+
             var aspect = _camera.aspect;
             var requiredSize = Mathf.Max(bounds.size.x / aspect, bounds.size.y) / 2f + _zoomPadding;
 
@@ -66,15 +96,31 @@ namespace Loadout
             }
         }
 
-        private void UpdateTargetLocalX()
+        private void UpdateTargetLocalPosition()
         {
+            if (_shopFocusActive)
+            {
+                var playerWorldPos = _playerEquipment.transform.position;
+                var targetWorldPos = new Vector3(
+                    (playerWorldPos.x + _shopFocusWorldPosition.x) / 2f,
+                    (playerWorldPos.y + _shopFocusWorldPosition.y) / 2f,
+                    0f
+                );
+                var localOffset = _camera.transform.parent.InverseTransformPoint(targetWorldPos);
+                _targetLocalX = localOffset.x;
+                _targetLocalY = localOffset.y;
+                return;
+            }
+
             if (_loadoutOffsetActive)
             {
                 _targetLocalX = _defaultLocalPosition.x + _loadoutCenterOffsetX;
+                _targetLocalY = _defaultLocalPosition.y;
             }
             else
             {
                 _targetLocalX = _defaultLocalPosition.x;
+                _targetLocalY = _defaultLocalPosition.y;
             }
         }
 
