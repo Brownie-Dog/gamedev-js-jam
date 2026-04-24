e247f243146e93b81473ae1c96780c355f5a48c3
Swxer <swxergamer64@gmail.com>
2026-04-24 18:58:00 +1000

    Use PascalCase for public variables

--- Assets/Scripts/Loadout/ItemFormationData.cs
+++ Assets/Scripts/Loadout/ItemFormationData.cs
@@ -7,46 +7,46 @@ public class ItemFormationData : ScriptableObject
     public class SlotGroup
     {
         [Header("Positions")] 
-        public Vector2 frontLeft;
-        public Vector2 frontRight;
-        public Vector2 backLeft;
-        public Vector2 backRight;
-        public Vector2 tail;
-        public Vector2 middle;
+        public Vector2 FrontLeft;
+        public Vector2 FrontRight;
+        public Vector2 BackLeft;
+        public Vector2 BackRight;
+        public Vector2 Tail;
+        public Vector2 Middle;
 
-        [Header("Rotations (Z-axis)")] [Range(-360, 360)]
-        public float frontLeftRot;
+        [Header("Rotations (Z-axis)")] 
+        [Range(-360, 360)] public float FrontLeftRot;
+        [Range(-360, 360)] public float FrontRightRot;
+        [Range(-360, 360)] public float BackLeftRot;
+        [Range(-360, 360)] public float BackRightRot;
+        [Range(-360, 360)] public float TailRot;
+        [Range(-360, 360)] public float MiddleRot;
 
-        [Range(-360, 360)] public float frontRightRot;
-        [Range(-360, 360)] public float backLeftRot;
-        [Range(-360, 360)] public float backRightRot;
-        [Range(-360, 360)] public float tailRot;
-        [Range(-360, 360)] public float middleRot;
-
-        [Header("Sorting Layers")] public int frontLeftLayer;
-        public int frontRightLayer;
-        public int backLeftLayer;
-        public int backRightLayer;
-        public int tailLayer;
-        public int middleLayer;
+        [Header("Sorting Layers")] 
+        public int FrontLeftLayer;
+        public int FrontRightLayer;
+        public int BackLeftLayer;
+        public int BackRightLayer;
+        public int TailLayer;
+        public int MiddleLayer;
 
         public SlotGroup(Vector2 fl, Vector2 fr, Vector2 bl, Vector2 br, Vector2 t, Vector2 m, int layer = 10)
         {
-            frontLeft = fl;
-            frontRight = fr;
-            backLeft = bl;
-            backRight = br;
-            tail = t;
-            middle = m;
-            frontLeftLayer = frontRightLayer = backLeftLayer = backRightLayer = tailLayer = middleLayer = layer;
-            frontLeftRot = frontRightRot = backLeftRot = backRightRot = tailRot = middleRot = 0;
+            FrontLeft = fl;
+            FrontRight = fr;
+            BackLeft = bl;
+            BackRight = br;
+            Tail = t;
+            Middle = m;
+            FrontLeftLayer = FrontRightLayer = BackLeftLayer = BackRightLayer = TailLayer = MiddleLayer = layer;
+            FrontLeftRot = FrontRightRot = BackLeftRot = BackRightRot = TailRot = MiddleRot = 0;
         }
     }
 
-    [Header("Directional Formations")] public SlotGroup upPositions;
-    public SlotGroup downPositions;
-    public SlotGroup leftPositions;
-    public SlotGroup rightPositions;
+    [Header("Directional Formations")] public SlotGroup UpPositions;
+    public SlotGroup DownPositions;
+    public SlotGroup LeftPositions;
+    public SlotGroup RightPositions;
 
     private void Reset()
     {
@@ -56,25 +56,25 @@ public class ItemFormationData : ScriptableObject
     [ContextMenu("Re-Initialize Values")]
     public void InitializeHardcodedValues()
     {
-        upPositions = new SlotGroup(
+        UpPositions = new SlotGroup(
             new Vector2(-0.5f, 0.6f), new Vector2(0.5f, 0.6f),
             new Vector2(-0.5f, -0.3f), new Vector2(0.5f, -0.3f),
             new Vector2(0f, -0.6f), new Vector2(0f, 0.2f)
         );
 
-        downPositions = new SlotGroup(
+        DownPositions = new SlotGroup(
             new Vector2(-0.6f, 0.06f), new Vector2(0.6f, 0.06f),
             new Vector2(-0.5f, 1f), new Vector2(0.5f, 1f),
             new Vector2(0f, 1.3f), new Vector2(0f, 0.7f)
         );
 
-        rightPositions = new SlotGroup(
+        RightPositions = new SlotGroup(
             new Vector2(0.5f, 0.7f), new Vector2(0.5f, 0.1f),
             new Vector2(-0.65f, 0.75f), new Vector2(-0.65f, 0.15f),
             new Vector2(-1f, 0.5f), new Vector2(-0.1f, 0.5f)
         );
 
-        leftPositions = new SlotGroup(
+        LeftPositions = new SlotGroup(
             new Vector2(-0.5f, 0.7f), new Vector2(-0.5f, 0.1f),
             new Vector2(0.65f, 0.75f), new Vector2(0.65f, 0.15f),
             new Vector2(1f, 0.5f), new Vector2(0.1f, 0.5f)
