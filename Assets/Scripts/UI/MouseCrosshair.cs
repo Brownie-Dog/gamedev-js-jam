15ed9943add0f7937b630271250b83305d189ffb
Matthew Lau <matthew.lau78@gmail.com>
2026-04-26 19:40:14 +1000

    Implement currency UI

--- Assets/Scripts/UI/MouseCrosshair.cs
+++ Assets/Scripts/UI/MouseCrosshair.cs
@@ -1,53 +1,55 @@
 using UnityEngine;
-using UnityEngine.Assertions;
 using UnityEngine.InputSystem;
 
-public class MouseCrosshair : MonoBehaviour
+namespace UI
 {
-    [SerializeField] private RectTransform _rectTransform;
-
-    [SerializeField] private float _rotationSpeed = 90f;
-
-    private void Awake()
+    public class MouseCrosshair : MonoBehaviour
     {
-        if (_rectTransform == null)
+        [SerializeField] private RectTransform _rectTransform;
+
+        [SerializeField] private float _rotationSpeed = 90f;
+
+        private void Awake()
         {
-            _rectTransform = GetComponent<RectTransform>();
-        }
-    }
-
-    private void OnEnable()
-    {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Confined;
-    }
-
-    private void OnDisable()
-    {
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
-    }
-
-    private void Update()
-    {
-        HandlePosition();
-        HandleRotation();
-    }
-
-    private void HandlePosition()
-    {
-        var mouse = Mouse.current;
-        if (mouse == null)
-        {
-            return;
+            if (_rectTransform == null)
+            {
+                _rectTransform = GetComponent<RectTransform>();
+            }
         }
 
-        _rectTransform.position = mouse.position.ReadValue();
-    }
+        private void OnEnable()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
 
-    private void HandleRotation()
-    {
-        // Rotate the crosshair over time around the Z axis
-        _rectTransform.Rotate(Vector3.forward, _rotationSpeed * Time.deltaTime);
+        private void OnDisable()
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        private void Update()
+        {
+            HandlePosition();
+            HandleRotation();
+        }
+
+        private void HandlePosition()
+        {
+            var mouse = Mouse.current;
+            if (mouse == null)
+            {
+                return;
+            }
+
+            _rectTransform.position = mouse.position.ReadValue();
+        }
+
+        private void HandleRotation()
+        {
+            // Rotate the crosshair over time around the Z axis
+            _rectTransform.Rotate(Vector3.forward, _rotationSpeed * Time.deltaTime);
+        }
     }
 }
